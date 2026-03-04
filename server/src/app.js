6caddeeb0ef565e14cbba97b6feafac82c04ff2a
whiplash24 <singhtanay010@gmail.com>
2026-03-04 15:39:57 +0530

    register public status routes in express app

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const monitorRoutes = require("./routes/monitorRoutes");
 const statsRoutes = require("./routes/statsRoutes");
+const publicRoutes = require("./routes/publicRoutes");
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get("/api/health", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/monitors", monitorRoutes);
 app.use("/api/stats", statsRoutes);
+app.use("/api/public", publicRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
